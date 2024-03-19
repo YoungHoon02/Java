613dac770de3b82abf12a98d9dce80b6b102f4cf
최영훈 <yh03011@naver.com>
2024-03-19 12:18:18 +0900

    modified:   YHworks/src/MultipleOfThree.java

--- YHworks/src/MultipleOfThree.java
+++ YHworks/src/MultipleOfThree.java
@@ -8,6 +8,14 @@ public class MultipleOfThree {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.print("숫자를 입력하세요: ");
+        value = input.nextInt();
+        
+        if(value % 3 == 0)
+            System.out.println("입력 받은 수는 3의 배수입니다.");
+
+        else
+            System.out.println("입력 받은 수는 3의 배수가 아닙니다.");
         
     }
 }
