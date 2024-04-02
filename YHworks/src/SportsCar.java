4b4021ea569d29166a42b57564fb8657e55f3764
최영훈 <yh03011@naver.com>
2024-04-02 11:04:26 +0900

    5주차 강의 저장

---
+++ YHworks/src/SportsCar.java
@@ -0,0 +1,5 @@
+//스포츠카는 Car를 상속받는다
+public class SportsCar extends Car{
+    
+    
+}
---
+++ YHworks/src/SportsCarTest.java
@@ -0,0 +1,9 @@
+public class SportsCarTest {
+    
+    public static void main(String[] args) {
+        
+        SportsCar obj1 = new SportsCar();
+
+        obj1.setColor("red");
+    }
+}
